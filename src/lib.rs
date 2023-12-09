9805c7aad4148fead56f44012020dcf7ef3b412c
jprugel <jonathon.prugel@gmail.com>
2023-12-08 23:28:10 -0500

    Switched to test based

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,38 @@
-// Purpose: Parts are not valid if they do not have an adjacent symbol that is not a "."
-pub fn get_sum_valid_parts(doc: &str) -> i32 {
-    let number_slices = get_number_slices(doc);
-    let ranges: Vec<(usize, usize)> = number_slices
-        .iter()
-        .map(|number| get_ranges_from_needle(doc, number))
-        .collect();
-    let long_ranges: Vec<(usize, usize)> = ranges.into_iter().map(expand_range).collect();
-    let filtered_by_symbols: Vec<(usize, usize)> = long_ranges
-        .into_iter()
-        .filter(|&range| {
-            (doc[range.0..range.1])
-                .chars()
-                .any(|c| !c.is_ascii_digit() && c != '.')
-        })
-        .collect();
-    dbg!(filtered_by_symbols);
-    doc.len() as i32
+use std::cmp::Ordering;
+
+pub fn get_present_range(needle: &str, haystack: &str) -> Option<(usize, usize)> {
+    let first = haystack.find(needle)?;
+    let last = first + needle.len() - 1;
+    Some((first, last))
 }
 
-pub fn get_number_slices(input: &str) -> Vec<String> {
-    let doc_parts: Vec<&str> = input.split('.').collect();
-    let filtered_symbols: Vec<String> = doc_parts
-        .iter()
-        .map(|&part| part.chars().filter(|&c| c.is_ascii_digit()).collect())
-        .collect();
-
-    filtered_symbols
-        .into_iter()
-        .filter(|part| part.parse::<i32>().is_ok())
-        .collect()
-}
-
-pub fn expand_range(input_range: (usize, usize)) -> (usize, usize) {
-    let mut output_range: (usize, usize) = (0, 0);
-    match input_range.0 {
-        0 => output_range.0 = 0,
-        _ => output_range.0 = input_range.0 - 1,
-    };
-    match input_range.1 % 12 {
-        0 => output_range.1 = input_range.1,
-        _ => output_range.1 = input_range.1 + 1,
-    };
-    output_range
-}
-
-pub fn get_ranges_from_needle(haystack: &str, needle: &str) -> (usize, usize) {
-    if let Some(start) = haystack.find(needle) {
-        (start, start + needle.len())
-    } else {
-        panic!("Number not found: {}", needle);
+pub fn get_past_range(needle: &str, haystack: &str) -> Option<(usize, usize)> {
+    let line_length = haystack.lines().nth(0)?.len();
+    let (present_first, present_last) = get_present_range(&needle, &haystack)?;
+    match present_first.cmp(&line_length) {
+        Ordering::Less => None,
+        _ => Some((present_first - line_length - 1, present_last - line_length - 1)),
     }
+}
+
+pub fn get_future_range(needle: &str, haystack: &str) -> Option<(usize, usize)> {
+    let line_length = haystack.lines().nth(0)?.len();
+    let doc_length = haystack.len();
+    let max_index = doc_length - line_length - 1;
+    let (present_first, present_last) = get_present_range(&needle, &haystack)?;
+    match present_first.cmp(&max_index) {
+        Ordering::Greater => None,
+        _ => Some((present_first + line_length + 1, present_last + line_length + 1)),
+    }
+}
+
+pub fn has_symbols(target: &str) -> bool {
+    target.contains(|c: char| !(c.is_digit(10) || c == '.'))
+}
+
+pub fn get_numbers(target: &str) -> Vec<&str> {
+    target
+        .split('.')
+        .filter(|&substring| !substring.is_empty() && !has_symbols(substring))
+        .collect()
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,20 +1,42 @@
 #[cfg(test)]
 pub mod tests {
-    use day_three::get_sum_valid_parts;
-
-    const DOC: &str = "467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..";
+    use day_three::{get_future_range, get_numbers, get_past_range, get_present_range, has_symbols};
 
     #[test]
-    fn part_one() {
-        assert_eq!(get_sum_valid_parts(DOC), 4361);
+    pub fn test_get_present_range() {
+        let doc: String = std::fs::read_to_string("../day_three/src/input.txt").expect("Failed to read document");
+        assert_eq!(get_present_range("114", doc.as_str()), Some((5, 7)));
+        assert_eq!(get_present_range("631", doc.as_str()), None);
+    }
+
+    #[test]
+    pub fn test_get_past_range() {
+        let doc: String = std::fs::read_to_string("../day_three/src/input.txt").expect("Failed to read document");
+        assert_eq!(get_past_range("114", doc.as_str()), None);
+        assert_eq!(get_past_range("631", doc.as_str()), None);
+        assert_eq!(get_past_range("35", doc.as_str()), Some((13, 14)));
+    }
+
+    #[test]
+    pub fn test_get_future_range() {
+        let doc: String = std::fs::read_to_string("../day_three/src/input.txt").expect("Failed to read document");
+        assert_eq!(get_future_range("114", doc.as_str()), Some((16, 18)));
+        assert_eq!(get_future_range("631", doc.as_str()), None);
+        assert_eq!(get_future_range("598", doc.as_str()), None);
+    }
+
+    #[test]
+    pub fn test_has_symbols() {
+        let doc: String = std::fs::read_to_string("../day_three/src/input.txt").expect("Failed to read document");
+        assert_eq!(has_symbols(doc.lines().nth(0).unwrap()), false);
+        assert_eq!(has_symbols(doc.lines().nth(1).unwrap()), true);
+    }
+
+    #[test]
+    pub fn test_get_numbers() {
+        let doc: String = std::fs::read_to_string("../day_three/src/input.txt").expect("Failed to read document");
+        assert_eq!(get_numbers(doc.lines().nth(0).unwrap()), ["467", "114"]);
+        assert_eq!(get_numbers(doc.lines().nth(1).unwrap()), Vec::<&str>::new());
+        assert_eq!(get_numbers(doc.lines().nth(2).unwrap()), ["35", "633"]);
     }
 }
